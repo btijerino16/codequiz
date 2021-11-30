ffb663c7ae6487efd7650c135a59e47884a01e43
btijerino16 <92263027+btijerino16@users.noreply.github.com>
2021-11-30 15:45:12 -0700

    adding pseudo code

--- assets/script.js
+++ assets/script.js
@@ -2,32 +2,54 @@ var startButton = document.getElementById('startgame')
 var quizContainer = document.getElementById('quiz-container')
 var quizBoxQuestions = document.getElementById('quiz-box-questions')
 var answerChoices = document.getElementById('answerbtns')
+var questions = document.getElementById('questions')
+
+
+
+var questions = [
+    {
+        one: "How can you see if a function is working?",
+        choices: ["console log", "windows", "github", "git status"],
+        answer: "alerts"
+    },
+    {
+        two: "How do you close out a function",
+        choices: ["curly braces", "quotations", "parentheses", "leave the program"],
+        answer: "parentheses"
+    },
+    {
+        three: "What does CSS stand for?",
+        choices: ["Waterfall", "Cascading Style Sheet", "the styling Duh", "what"],
+        answer: "Cascading Style Sheet"
+    },
+    {
+        four: "What attribute do you use for an image?",
+        choices: ["SRC", "IMG", "REF", "LOVE"],
+        answer: "IMG"
+    },
+
+];
+
+
 
 function startQuiz() {
-    console.log("sayhello");
-
+    // when user clicks start 1. the start button will hide 2. the timer starts 3. first question populate
 
 }
+
 
 function clickNext() {
 
-
-
-}
-
-
-
-
-
-
-function nextQuestion() {
-
+    //the next question and possible answers populate
 
 
 
 }
 
-function answerChoice() {
+
+
+function timeInterval() {
+    // when this function starts, it will start a timer on the quiz
 
 
 
