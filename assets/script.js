ab9f2a9ccac6d0167c1043a3490b2c4641f3a654
btijerino16 <92263027+btijerino16@users.noreply.github.com>
2021-11-30 12:52:49 -0700

    started javascript

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+var startButton = document.getElementById('startgame')
+
+function startGame () {
+    console.log("it works")
+    
+
+
+}
+
+function nextQuestion () {
+
+
+
+
+}
+
+function answerChoice () {
+
+
+
+}
+
+
+startButton.addEventListener('click' , startGame)
