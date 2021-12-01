425a05f5266902ea4fc5aabd013ffbec33b189f7
btijerino16 <92263027+btijerino16@users.noreply.github.com>
2021-11-30 22:52:42 -0700

    cleared up duplicates in html, set up timer

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,86 @@
-var startButton = document.getElementById('startgame')
-var quizContainer = document.getElementById('quiz-container')
-var quizBoxQuestions = document.getElementById('quiz-box-questions')
-var answerChoices = document.getElementById('answerbtns')
-var questions = document.getElementById('questions')
+var startButton = document.getElementById('start')
+var next = document.getElementById('next-button')
+var quizBoxQuestions = document.getElementById('quiz-container')
+var answerChoices = document.getElementById('btn')
 
 
 
-
-var questions = [
-    {
-        one: "How can you see if a function is working?",
-        choices: ["console log", "windows", "github", "git status"],
-        answer: "alerts"
-    },
-    {
-        two: "How do you close out a function",
-        choices: ["curly braces", "quotations", "parentheses", "leave the program"],
-        answer: "parentheses"
-    },
-    {
-        three: "What does CSS stand for?",
-        choices: ["Waterfall", "Cascading Style Sheet", "the styling Duh", "what"],
-        answer: "Cascading Style Sheet"
-    },
-    {
-        four: "What attribute do you use for an image?",
-        choices: ["SRC", "IMG", "REF", "LOVE"],
-        answer: "IMG"
-    },
-
-];
-
+var highScoreBtn = document.querySelector("#scores");
 
 
 function startQuiz() {
     // when user clicks start 1. the start button will hide 2. the timer starts 3. first question populate
 
     startButton.classList.add('hide')
-    setInterval(questions)
-
-
-}
-
-function displayQuestions ()
-{
-
+    myTimer()
+    displayQuestions()
 
 }
 
-function clickNext() {
+var sec = 12;
+var time = setInterval(myTimer, 1000);
+
+
+function myTimer() {
+   document.getElementById('timer').innerHTML = sec + "sec left";
+
+   sec--;
+   if (sec == -1) {
+      clearInterval(time);
+       alert("Out of time");
+       
+       reset;   
+   }
+}
+
+
+function displayQuestions () {
+   
+
+}
+
+    var quizQuestions = [
+        {
+            quiz: "How can you see if a function is working?",
+            choices: ["console log", "windows", "github", "git status"],
+            answer: "alerts"
+        },
+        {
+            quiz: "How do you close out a function",
+            choices: ["curly braces", "quotations", "parentheses", "leave the program"],
+            answer: "parentheses"
+        },
+        {
+            quiz: "What does CSS stand for?",
+            choices: ["Waterfall", "Cascading Style Sheet", "the styling Duh", "what"],
+            answer: "Cascading Style Sheet"
+        },
+        {
+        quiz: "What attribute do you use for an image?",
+            choices: ["SRC", "IMG", "REF", "LOVE"],
+            answer: "IMG"
+        },
+    
+    ];
+    
+      
+
+
+
+
+    function showQuestion() {
 
     //the next question and possible answers populate
 
 
-
 }
 
 
 
-function timeInterval() {
-    // when this function starts, it will start a timer on the quiz, deduct if answer wrong, add time if answer right
 
 
 
-}
+startButton.addEventListener('click', startQuiz,)
+next.addEventListener('click', displayQuestions)
 
 
-startButton.addEventListener('click', startQuiz)
